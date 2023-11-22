654439f56c5d7b10059ad4dd74c848b99d768204
futao <futao@mysteel.com>
2023-11-22 16:43:21 +0800

    dubbo template

--- basic-restful/src/main/java/com/idle/fish/template/basic/restful/RestResultWrapper.java
+++ basic-restful/src/main/java/com/idle/fish/template/basic/restful/RestResultWrapper.java
@@ -1,7 +1,6 @@
 package com.idle.fish.template.basic.restful;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -32,7 +31,7 @@ public class RestResultWrapper implements ResponseBodyAdvice<Object> {
             return o;
         }
         if (o instanceof String || aClass.isAssignableFrom(StringHttpMessageConverter.class)) {
-            return JSON.toJSONString(RestResult.simpleSuccess(o), SerializerFeature.WriteNullListAsEmpty);
+            return JSON.toJSONString(RestResult.simpleSuccess(o));
         }
         return RestResult.simpleSuccess(o);
     }
--- basic-user/src/main/java/com/idle/fish/tamplate/basic/user/BasicUser.java
+++ basic-user/src/main/java/com/idle/fish/tamplate/basic/user/BasicUser.java
@@ -1,6 +1,6 @@
 package com.idle.fish.tamplate.basic.user;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.idle.fish.template.basic.db.entity.base.BaseIdUserTime;
 import lombok.Getter;
 import lombok.Setter;
