fa750c82cbc15acad3f812a77752316a18245b47
idle fish <FutaoSmile@gmail.com>
2023-11-29 21:25:07 +0800

    二维码与条形码

---
+++ basic-util/src/main/java/com/idle/fish/template/basic/util/BarcodeGeneratorUtils.java
@@ -0,0 +1,48 @@
+package com.idle.fish.template.basic.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 条形码工具类
+ *
+ * @author idle fish
+ * @since 2023/11/29
+ */
+@Slf4j
+@UtilityClass
+public class BarcodeGeneratorUtils {
+    // 生成条形码的方法
+    public static void generateBarcode(String data, int width, int height, String filePath) {
+        try {
+            Map<EncodeHintType, Object> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8"); // 设置字符编码
+
+            MultiFormatWriter writer = new MultiFormatWriter();
+            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.CODE_128, width, height, hints);
+
+            // 创建BufferedImage对象来表示条形码
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    image.setRGB(x, y, bitMatrix.get(x, y) ? 0 : 0xFFFFFF); // 生成黑色条和白色背景的条形码
+                }
+            }
+            // 将条形码保存到文件
+            File barcodeFile = new File(filePath);
+            ImageIO.write(image, "png", barcodeFile);
+        } catch (Exception e) {
+            log.error("生成条形码失败", e);
+        }
+    }
+}
